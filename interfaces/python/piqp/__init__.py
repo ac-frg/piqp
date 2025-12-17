f64a3547a76de559f0924e7ced2d31fd1e269268
Tudor Oancea <oancea.tudor@icloud.com>
2025-12-17 16:40:29 +0100

    add piqp headers and libs to python wheels

--- interfaces/python/piqp/__init__.py
+++ interfaces/python/piqp/__init__.py
@@ -7,6 +7,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from . import instruction_set
+import os
 
 
 def load_main_module(globals):
@@ -31,6 +32,39 @@ def load_main_module(globals):
             return
 
     assert load_module("piqp_python")
+
+
+def get_include():
+    """Return the directory containing piqp C++ headers.
+
+    Returns
+    -------
+    str
+        Absolute path to the directory containing piqp headers.
+    """
+    return os.path.join(os.path.dirname(__file__), "include")
+
+
+def get_library_dir():
+    """Return the directory containing piqp shared libraries.
+
+    Returns
+    -------
+    str
+        Absolute path to the directory containing piqp shared libraries.
+    """
+    return os.path.join(os.path.dirname(__file__), "lib")
+
+
+def get_cmake_dir():
+    """Return the directory containing piqp CMake config files.
+
+    Returns
+    -------
+    str
+        Absolute path to the directory containing CMake config files.
+    """
+    return os.path.join(os.path.dirname(__file__), "cmake")
 
 
 load_main_module(globals=globals())
